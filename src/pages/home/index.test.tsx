fe8aafb192e8c9ba39bd0b69adad5407f8026c9f
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-24 20:59:10 +0700

    improve: add unit test

---
+++ src/pages/home/index.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { HomePage } from "./";
+
+describe("Test HomePage", () => {
+  it("renders the correct heading text", () => {
+    const { getByText } = render(<HomePage />);
+    const headingElement = getByText("Waduh");
+    expect(headingElement).toBeInTheDocument();
+  });
+});
